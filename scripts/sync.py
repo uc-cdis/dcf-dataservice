63870ebfcab26f37dc9d0730857c37a1facf5515
Giang Bui <giangbui@uchicago.edu>
2018-05-19 06:48:22 -0500

    feat(multiple_thread): add multiple threads

--- scripts/sync.py
+++ scripts/sync.py
@@ -5,59 +5,135 @@ import requests
 import sys
 import getopt
 import argparse
+import Queue
+import threading
+import time
 
 from cdislogging import get_logger
 
 from indexclient.client import IndexClient
 from dev_settings import SIGNPOST
 
+THREAD_NUM = 2
+
+global exitFlag
+exitFlag = 0
+
 logger = get_logger("ReplicationThread")
 
 indexclient = IndexClient(SIGNPOST['host'], SIGNPOST['version'], SIGNPOST['auth'])
 
-class AMZBucketReplication(object):
+queueLock = threading.Lock()
+workQueue = Queue.Queue()
+
+def get_file_from_uuid(uuid):
+    '''
+    get document from indexd with provided uuid
+    '''
+    doc = None
+    if uuid:
+        doc = indexclient.get(uuid)
+    return doc
+
+def create_index(**kwargs):
+    return indexclient.create(**kwargs)
+
+def get_fileinfo_list_from_manifest(manifest_file):
+    """
+    get list of dictionaries from manifest file.
+    [
+        {
+            'did':'11111111111111111',
+            'filename': 'abc.bam',
+            'size': 1,
+            'hash': '1223344543t34mt43tb43ofh',
+            'metadata': 'abcxyz'
+        },
+    ]
+    """
+    fake = {
+            'did':'11111111111111111',
+            'filename': 'abc.bam',
+            'size': 1,
+            'hash': '1223344543t34mt43tb43ofh'}
+
+    l = [fake,fake,fake,fake,fake,fake,fake,fake,fake,fake,fake,fake,fake,fake,fake,fake]
+
+    #with open(manifest_file) as f:
+    #    read(f)
+    return l
+
+def call_aws_copy(fi, from_bucket, to_bucket):
+    #execstr = "aws s3 sync s3://{} s3://{} --exclude \"*\"".format(from_bucket, to_bucket)
+    #execstr = execstr + " --include \"{}/{}\"".format(fi.get("did"), fi.get("filename"))
+    #os.system(execstr)
+    print("running the job on {}".format(fi.get("did")))
+    time.sleep(2)
+
+def process_data(threadName, from_bucket, to_bucket, q):
+    while not exitFlag:
+        queueLock.acquire()
+        if not q.empty():
+            fi = q.get()
+            queueLock.release()
+            print "%s processing %s" % (threadName, fi.get("did"))
+            #call_aws_copy(fi, from_bucket, to_bucket)
+        else:
+            queueLock.release()
+            #time.sleep(5)
+
+class singleThread(threading.Thread):
+    def __init__(self, threadID, threadName, from_bucket, to_bucket, q):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.threadName = threadName
+        self.from_bucket = from_bucket
+        self.to_bucket = to_bucket
+        self.q = q
+
+    def run(self):
+        print "Starting " + self.name
+        process_data(self.threadName, self.from_bucket, self.to_bucket, self.q)
+        print "Exiting " + self.name
+
+
+class AWSBucketReplication(object):
 
     def __init__(self, from_bucket, to_bucket, manifest_file):
         self.from_bucket = from_bucket
         self.to_bucket = to_bucket
         self.manifest_file = manifest_file
+        self.thread_list = []
+        for i in xrange(0,THREAD_NUM):
+            thread = singleThread(str(i), 'thread_{}'.format(i), self.from_bucket, self.to_bucket, workQueue)
+            self.thread_list.append(thread)
 
-    def get_file_from_uuid(self, uuid):
-        doc = None
+    def prepare(self):
+        """
+        concurently process a set of data files.
+        """
+        submitting_files = get_fileinfo_list_from_manifest(self.manifest_file)
+        for th in self.thread_list:
+            th.start()
+        queueLock.acquire()
+        for fi in submitting_files:
+            workQueue.put(fi)
+        queueLock.release()
 
-        if uuid:
-            doc = self.indexclient.get(uuid)
-        return doc
+    def run(self):
 
-    def create_index(self, **kwargs):
-        return indexclient.create(**kwargs)
+        # Wait for queue to empty
+        while not workQueue.empty():
+            pass
 
-    def get_fileinfo_list_from_manifest(self):
-        l = []
-        with open(self.manifest_file) as f:
-            read(f)
-        return l
+        # Notify threads it's time to exit
+        global exitFlag
+        exitFlag = 1
 
-    def call_aws_copy(self, fileinfo):
-        execstr = ("aws s3 cp {}/{}/{} {}", self.from_bucket, fileinfo['uuid'], fileinfo.get('filename',''), self.to_bucket)
-        os.system(execstr)
-
-    def run(self, mode="test"):
-        logger.info("starting ....")
-        file_list = self.get_fileinfo_list_from_manifest()
-        for fileinfo in file_list:
-            document = self.get_file_from_uuid(fileinfo.get("did"), None)
-            if document is None:
-                logger.info('copy file location in from_bucket to to-bucket')
-                self.call_aws_copy(fileinfo)
-
-                logger.info("update indexd with new uuid {}".format(fileinfo["did"]))
-                # IndexClient
-                self.create_index(did=fileinfo.get("did"),
-                                   hashes=fileinfo.get('hashes',''),
-                                   size=fileinfo.get('size',0),
-                                   urls=fileinfo.get('urls',[]),
-                                   metadata=fileinfo.get('metadata',''))
+        # Wait for all threads to complete
+        for t in self.thread_list:
+            t.join()
+        print "Done"
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
@@ -70,7 +146,9 @@ def parse_arguments():
     return parser.parse_args()
 
 if __name__ == "__main__":
-    args = parse_arguments()
-
-    if args.action == 'sync':
-        print "sync from gdc aws bucket to gen3 dcf bucket"
+    #args = parse_arguments()
+    aws = AWSBucketReplication('from','to','test')
+    aws.prepare()
+    aws.run()
+    #if args.action == 'sync':
+    #    print "sync from gdc aws bucket to gen3 dcf bucket"
--- tests/test_sync.py
+++ tests/test_sync.py
@@ -5,35 +5,40 @@ from mock import patch
 from scripts.sync import AMZBucketReplication
 
 TEST_UUID = '11111111111111111111111111111111'
+TEST_FILENAME = 'test'
 TEST_URL = ['test_url1', 'test_url2']
+
+@patch('scripts.sync.AMZBucketReplication.get_submitting_indexd_files')
+@patch('scripts.sync.AMZBucketReplication.get_file_from_uuid')
+def test_uuid_not_existed(get_index_uuid, get_submitting_files):
+    """
+    in case that uuid is not existed, copy object from backup bucket to dcf bucket and update indexd
+    """
+    document = MagicMock()
+    document.did = TEST_UUID
+    document.urls = TEST_URL
+    document.filename = TEST_FILENAME
+    get_index_uuid.return_value = document
+    get_submitting_files.return_value = [{'did':TEST_UUID, 'filename': TEST_FILENAME}]
+
+    instance = AMZBucketReplication('test1','test2','manifest')
+    instance.create_index = MagicMock()
+    instance.call_aws_copy = MagicMock()
+
+    instance.run()
+    assert instance.create_index.called == True
+    assert instance.call_aws_copy.called == True
 
 @patch('scripts.sync.AMZBucketReplication.get_fileinfo_list_from_manifest')
 @patch('scripts.sync.AMZBucketReplication.get_file_from_uuid')
-def test_file_already_existed(get_index_uuid, get_fileinfo_list, monkeypatch):
+def test_uuid_is_existed(get_index_uuid, get_fileinfo_list, monkeypatch):
 
     document = MagicMock()
     document.did = TEST_UUID
     document.urls = TEST_URL
+    document.filename = TEST_FILENAME
     get_index_uuid.return_value = document
-    get_fileinfo_list.return_value = []
-
+    get_fileinfo_list.return_value = [{'did':TEST_UUID, 'filename': TEST_FILENAME}]
     instance = AMZBucketReplication('test1','test2','manifest')
-    instance.create_index = MagicMock()
-    instance.call_aws_copy = MagicMock()
-    instance.run()
-    assert instance.create_index.called == False
-    assert instance.call_aws_copy.called == False
-
-@patch('scripts.sync.AMZBucketReplication.get_fileinfo_list_from_manifest')
-@patch('scripts.sync.AMZBucketReplication.get_file_from_uuid')
-def test_updat4e_indexd_with_new_file(get_index_uuid, get_fileinfo_list, monkeypatch):
-
-    get_index_uuid.return_value = None
-    get_fileinfo_list.return_value = [{'did':TEST_UUID}]
-
-    instance = AMZBucketReplication('test1','test2','manifest')
-    instance.create_index = MagicMock()
-    instance.call_aws_copy = MagicMock()
-    instance.run()
-    assert instance.create_index.called == True
-    assert instance.call_aws_copy.called == True
+    submitting_files = instance.get_submitting_indexd_files()
+    assert len(submitting_files) == 0
