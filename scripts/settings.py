c01784415cba95f55dffee93894d5f9b66471a51
Giang Bui <giangbui@uchicago.edu>
2018-06-10 19:10:21 -0500

    chore(json): add json config

--- scripts/settings.py
+++ scripts/settings.py
@@ -1,20 +1,20 @@
 import os
+import json
 
-PROJECT_MAP = {
-        'TGCA': 'tcga-xxxx-phs000178',
-        'TARGET': 'target-xxxx-phs000218',
-        'FM': 'fm-xxxx-phs000179',
-        'CCLE': 'ccle-xxx'
-        }
+# Signpost
+#SIGNPOST = {
+#  'host': 'http://localhost:8000',
+#  'version': 'v0',
+#  'auth': None}
+#
+#PROJECT_MAP = {
+#       'TGCA': 'tcga-xxxx-phs000178',
+#       'TARGET': 'target-xxxx-phs000218',
+#       'FM': 'fm-xxxx-phs000179',
+#       'CCLE': 'ccle-xxx'
+#        }
 
-SIGNPOST={'host': '',
-          'version': '',
-          'auth': None}
-
-dir_path = "/secrets"
-data_service_creds = os.path.join(dir_path, 'dataservice_credentials.json')
-
-if os.path.exists(data_service_creds):
-    with open(data_service_creds, 'r') as f:
-        data = json.load(f)
-        SIGNPOST = data.get("SIGNPOST", {})
+with open('/secrets/dcf_dataservice_credentials.json','r') as f:
+    data = json.loads(f.read())
+    PROJECT_MAP = data.get('PROJECT_MAP',{})
+    SIGNPOST = data.get('SIGNPOST',{})
