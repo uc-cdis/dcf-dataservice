83bafce95833102104a0c69eaeb61243f61d016d
Edward Malinowski <edwardmalinowski@Edwards-MacBook-Pro.local.cdis>
2019-12-05 09:25:15 -0600

    feat(configurable-config): added flag to specify config directory to allow multiple configurations

--- jobs/kube-setup-scripts.sh
+++ jobs/kube-setup-scripts.sh
@@ -4,37 +4,52 @@
 #
 set -e
 
+while getopts :d: option
+do
+    case "$option" in
+    d)
+         config_directory=$2
+         ;;
+         esac
+done
+
+
+if [ -z "$config_directory" ]
+then
+  config_directory="dcf_dataservice"
+fi
+
 source "${GEN3_HOME}/gen3/lib/utils.sh"
 gen3_load "gen3/lib/kube-setup-init"
 
-if [[ -d ${WORKSPACE}/${vpc_name}/apis_configs/dcf_dataservice ]]; then
+if [[ -d ${WORKSPACE}/${vpc_name}/apis_configs/${config_directory} ]]; then
   if ! g3kubectl get secret dcf-aws-creds-secret > /dev/null 2>&1; then
-     g3kubectl create secret generic dcf-aws-creds-secret --from-file=credentials=${WORKSPACE}/${vpc_name}/apis_configs/dcf_dataservice/aws_creds_secret
+     g3kubectl create secret generic dcf-aws-creds-secret --from-file=credentials=${WORKSPACE}/${vpc_name}/apis_configs/${config_directory}/aws_creds_secret
   fi
 
   if ! g3kubectl get secret google-creds-secret > /dev/null 2>&1; then
-     g3kubectl create secret generic google-creds-secret --from-file=google_service_account_creds=${WORKSPACE}/${vpc_name}/apis_configs/dcf_dataservice/gcloud-creds-secret
+     g3kubectl create secret generic google-creds-secret --from-file=google_service_account_creds=${WORKSPACE}/${vpc_name}/apis_configs/${config_directory}/gcloud-creds-secret
   fi
 
   if ! g3kubectl get secrets/dcf-dataservice-json-secret > /dev/null 2>&1; then
-     if [[ ! -f "${WORKSPACE}/${vpc_name}/apis_configs/dcf_dataservice/creds.json" ]]; then
-         touch "${WORKSPACE}/${vpc_name}/apis_configs/dcf_dataservice/creds.json"
+     if [[ ! -f "${WORKSPACE}/${vpc_name}/apis_configs/${config_directory}/creds.json" ]]; then
+         touch "${WORKSPACE}/${vpc_name}/apis_configs/${config_directory}/creds.json"
      fi
-     echo "create dcf-dataservice-json-secret using current creds file apis_configs/dcf_dataservice/creds.json"
-     g3kubectl create secret generic dcf-dataservice-json-secret --from-file=dcf_dataservice_credentials.json=${WORKSPACE}/${vpc_name}/apis_configs/dcf_dataservice/creds.json
+     echo "create dcf-dataservice-json-secret using current creds file apis_configs/${config_directory}/creds.json"
+     g3kubectl create secret generic dcf-dataservice-json-secret --from-file=dcf_dataservice_credentials.json=${WORKSPACE}/${vpc_name}/apis_configs/${config_directory}/creds.json
   fi
 
   if ! g3kubectl get secrets/dcf-dataservice-settings-secrets > /dev/null 2>&1; then
       if [[ ! -f "${WORKSPACE}/${vpc_name}/apis_configs/dcf_dataservice_settings" ]]; then
           touch "${WORKSPACE}/${vpc_name}/apis_configs/dcf_dataservice_settings"
       fi
-      g3kubectl create secret generic dcf-dataservice-settings-secrets --from-file=dcf_dataservice_settings=${WORKSPACE}/${vpc_name}/apis_configs/dcf_dataservice/dcf_dataservice_settings
+      g3kubectl create secret generic dcf-dataservice-settings-secrets --from-file=dcf_dataservice_settings=${WORKSPACE}/${vpc_name}/apis_configs/${config_directory}/dcf_dataservice_settings
    fi
 
   if ! g3kubectl get configmaps/project-map-manifest > /dev/null 2>&1; then
-     if [[ ! -f "${WORKSPACE}/${vpc_name}/apis_configs/dcf_dataservice/GDC_project_map.json" ]]; then
-       touch "${WORKSPACE}/${vpc_name}/apis_configs/dcf_dataservice/GDC_project_map.json"
+     if [[ ! -f "${WORKSPACE}/${vpc_name}/apis_configs/${config_directory}/GDC_project_map.json" ]]; then
+       touch "${WORKSPACE}/${vpc_name}/apis_configs/${config_directory}/GDC_project_map.json"
      fi
-     g3kubectl create configmap project-map-manifest --from-file=GDC_project_map.json=${WORKSPACE}/${vpc_name}/apis_configs/dcf_dataservice/GDC_project_map.json
+     g3kubectl create configmap project-map-manifest --from-file=GDC_project_map.json=${WORKSPACE}/${vpc_name}/apis_configs/${config_directory}/GDC_project_map.json
   fi
 fi
