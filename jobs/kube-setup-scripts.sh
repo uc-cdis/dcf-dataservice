8588fdb825bbfab97cdd991905b9a97df797753a
Giang Bui <giangbui0816@gmail.com>
2020-05-21 12:24:09 -0500

    feat(setup):make easier setup

--- jobs/kube-setup-scripts.sh
+++ jobs/kube-setup-scripts.sh
@@ -8,33 +8,15 @@ source "${GEN3_HOME}/gen3/lib/utils.sh"
 gen3_load "gen3/lib/kube-setup-init"
 
 if [[ -d ${WORKSPACE}/${vpc_name}/apis_configs/dcf_dataservice ]]; then
-  if ! g3kubectl get secret dcf-aws-creds-secret > /dev/null 2>&1; then
-     g3kubectl create secret generic dcf-aws-creds-secret --from-file=credentials=${WORKSPACE}/${vpc_name}/apis_configs/dcf_dataservice/aws_creds_secret
-  fi
+  g3kubectl delete secret dcf-aws-creds-secret
+  g3kubectl delete secret google-creds-secret
+  g3kubectl delete secret dcf-dataservice-json-secret
+  g3kubectl delete secret dcf-dataservice-settings-secrets
+  g3kubectl delete configmap project-map-manifest
 
-  if ! g3kubectl get secret google-creds-secret > /dev/null 2>&1; then
-     g3kubectl create secret generic google-creds-secret --from-file=google_service_account_creds=${WORKSPACE}/${vpc_name}/apis_configs/dcf_dataservice/gcloud-creds-secret
-  fi
-
-  if ! g3kubectl get secrets/dcf-dataservice-json-secret > /dev/null 2>&1; then
-     if [[ ! -f "${WORKSPACE}/${vpc_name}/apis_configs/dcf_dataservice/creds.json" ]]; then
-         touch "${WORKSPACE}/${vpc_name}/apis_configs/dcf_dataservice/creds.json"
-     fi
-     echo "create dcf-dataservice-json-secret using current creds file apis_configs/dcf_dataservice/creds.json"
-     g3kubectl create secret generic dcf-dataservice-json-secret --from-file=dcf_dataservice_credentials.json=${WORKSPACE}/${vpc_name}/apis_configs/dcf_dataservice/creds.json
-  fi
-
-  if ! g3kubectl get secrets/dcf-dataservice-settings-secrets > /dev/null 2>&1; then
-      if [[ ! -f "${WORKSPACE}/${vpc_name}/apis_configs/dcf_dataservice_settings" ]]; then
-          touch "${WORKSPACE}/${vpc_name}/apis_configs/dcf_dataservice_settings"
-      fi
-      g3kubectl create secret generic dcf-dataservice-settings-secrets --from-file=dcf_dataservice_settings=${WORKSPACE}/${vpc_name}/apis_configs/dcf_dataservice/dcf_dataservice_settings
-   fi
-
-  if ! g3kubectl get configmaps/project-map-manifest > /dev/null 2>&1; then
-     if [[ ! -f "${WORKSPACE}/${vpc_name}/apis_configs/dcf_dataservice/GDC_project_map.json" ]]; then
-       touch "${WORKSPACE}/${vpc_name}/apis_configs/dcf_dataservice/GDC_project_map.json"
-     fi
-     g3kubectl create configmap project-map-manifest --from-file=GDC_project_map.json=${WORKSPACE}/${vpc_name}/apis_configs/dcf_dataservice/GDC_project_map.json
-  fi
+  g3kubectl create secret generic dcf-aws-creds-secret --from-file=credentials=${WORKSPACE}/${vpc_name}/apis_configs/dcf_dataservice/aws_creds_secret
+  g3kubectl create secret generic google-creds-secret --from-file=google_service_account_creds=${WORKSPACE}/${vpc_name}/apis_configs/dcf_dataservice/gcloud-creds-secret
+  g3kubectl create secret generic dcf-dataservice-json-secret --from-file=dcf_dataservice_credentials.json=${WORKSPACE}/${vpc_name}/apis_configs/dcf_dataservice/creds.json
+  g3kubectl create secret generic dcf-dataservice-settings-secrets --from-file=dcf_dataservice_settings=${WORKSPACE}/${vpc_name}/apis_configs/dcf_dataservice/dcf_dataservice_settings
+  g3kubectl create configmap project-map-manifest --from-file=GDC_project_map.json=${WORKSPACE}/${vpc_name}/apis_configs/dcf_dataservice/GDC_project_map.json
 fi
