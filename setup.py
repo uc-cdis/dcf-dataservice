2a1339cff9fc129dd946634dd030b3cf4a7bbffc
MaribelleHGomez <maribellehgomez@gmail.com>
2021-12-14 10:48:24 -0600

    change versions of packages, including beam due to error during pip install from old beam package

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from __future__ import print_function
 import setuptools
 
 REQUIRED_PACKAGES = [
-    "python_dateutil==2.8.0",
-    "requests>=2.18.0<3.0.0",
-    "boto3>=1.9.111",
+    "python_dateutil==2.8.2",
+    "requests>=2.26.0<3.0.0",
+    "boto3==1.20.24",
     "google-cloud==0.34.0",
-    "google-resumable-media==0.3.1",
-    "google-cloud-storage==1.6.0",
-    "apache-beam[gcp]==2.16.0",
-    "setuptools==40.3.0",
+    "google-resumable-media==2.1.0",
+    "google-cloud-storage==1.43.0",
+    "apache-beam[gcp]==2.34.0",
+    "setuptools==59.6.0",
 ]
 
 PACKAGE_NAME = "scripts"
