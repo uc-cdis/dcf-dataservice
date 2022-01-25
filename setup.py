df0e57f64cbc7a0c2bbcf67720b98c924b3f0710
BinamB <numb.baj@gmail.com>
2022-01-25 13:19:05 -0600

    pin deps

--- setup.py
+++ setup.py
@@ -4,17 +4,19 @@ from __future__ import print_function
 import setuptools
 
 REQUIRED_PACKAGES = [
-    "python_dateutil>=2.8.0<3.0.0",
-    "requests>=2.20.0<3.0.0",
+    "python_dateutil==2.8.2",
+    "requests==2.26.0",
     "boto3>=1.9.111<2.0.0",
     "retry<=0.9.2",
-    "google-auth>=1.4.1<2.0.0",
+    "google-auth==1.35.0",
     "google-cloud>=0.34.0<1.0.0",
-    "google-resumable-media>=0.3.1<1.0.0",
+    "google-resumable-media==2.1.0",
     "google-cloud-storage>=1.6.0<2.0.0",
-    "httplib2<1.0.0",
-    "pyparsing>-2.4.7<3.0.0" "apache-beam[gcp]>=2.16.0<3.0.0",
-    "setuptools>=40.3.0<41.0.0",
+    "httplib2==0.19.1",
+    "pyparsing==2.4.7",
+    "apache-beam[gcp]==2.35.0",
+    "urllib3==1.26.7",
+    "setuptools==57.5.0",
 ]
 
 PACKAGE_NAME = "scripts"
