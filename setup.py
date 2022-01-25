3c4ac8f11488cd5911c0c7a17a40bfa643c0d348
BinamB <numb.baj@gmail.com>
2022-01-25 14:04:21 -0600

    downgrade apache

--- setup.py
+++ setup.py
@@ -3,20 +3,22 @@ from __future__ import print_function
 
 import setuptools
 
+# NOTE: apache-beam sdk requires some packages to have specific versions
+# More details here: https://cloud.google.com/dataflow/docs/concepts/sdk-worker-dependencies#python-3.7.12
 REQUIRED_PACKAGES = [
-    "python_dateutil==2.8.2",
-    "requests==2.26.0",
+    "python_dateutil==2.8.0",
+    "requests==2.22.0",
     "boto3>=1.9.111<2.0.0",
     "retry<=0.9.2",
-    "google-auth==1.35.0",
-    "google-cloud>=0.34.0<1.0.0",
-    "google-resumable-media==2.1.0",
-    "google-cloud-storage>=1.6.0<2.0.0",
-    "httplib2==0.19.1",
-    "pyparsing==2.4.7",
-    "apache-beam[gcp]==2.35.0",
-    "urllib3==1.26.7",
-    "setuptools==57.5.0",
+    "google-auth==1.6.3",
+    "google-cloud>0.0.0<1.0.0",
+    "google-resumable-media==0.4.1",
+    "google-cloud-storage>=1.0.0<2.0.0",
+    "httplib2==0.12.0",
+    "pyparsing==2.4.2",
+    "apache-beam[gcp]==2.16.0",
+    "urllib3==1.25.6",
+    "setuptools==41.4.0",
 ]
 
 PACKAGE_NAME = "scripts"
