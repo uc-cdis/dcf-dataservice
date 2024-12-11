215c7d21bd7d09feab4aeb826b97fcd635a9fe3e
BinamB <numb.baj@gmail.com>
2024-12-11 00:24:59 -0600

    Fix settings

--- scripts/settings.py
+++ scripts/settings.py
@@ -17,10 +17,9 @@ try:
 except Exception as e:
     print("Can not read dcf_dataservice_credentials.json file. Detail {}".format(e))
 
-
-# PROJECT_ACL = {}
+PROJECT_ACL = {}
 try:
-    with open("/dcf-dataservice/GDC_project_map.json", "r") as f:
+    with open("/dcf_dataservice/GDC_project_map.json", "r") as f:
         PROJECT_ACL = json.loads(f.read())
 except Exception as e:
     print("Can not read GDC_project_map.json file. Detail {}".format(e))
